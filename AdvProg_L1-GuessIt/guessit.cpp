09118f83ca2ffd65dac3bdfdc3ddc2b055915292
TrMiDuc <124168615+TrMiDuc@users.noreply.github.com>
2023-03-31 11:48:39 +0900

    Update guessit.cpp

--- AdvProg_L1-GuessIt/guessit.cpp
+++ AdvProg_L1-GuessIt/guessit.cpp
@@ -8,27 +8,27 @@ using namespace std;
 
 /***
     Args:
-        
     Returns:
         number (int) : random number in range 1-100
 ***/
 int generateRandomNumber() {
     // TODO: Return the random number in range 1 to 100
-    int randomNumber = 1 + rand() % 100;
-    return randomNumber;
+
+    return (int) rand()%100;
 }
 
 
 /***
     Args:
-        
     Returns:
         number (int) : the number that player guessed
 ***/
 int getPlayerGuess() {
     // TODO: Ask the player guest and return the player's number
-    int number; cin >> number;
-    return num;
+
+    int guess;
+    cin >> guess;
+    return guess;
 }
 
 
@@ -40,16 +40,21 @@ int getPlayerGuess() {
         answer (string) : answer of computer after checking result
 ***/
 string getAnswer(int number, int randomNumber) {
-    /*** 
+    /***
         TODO: check number with randomNumber and return the result.
               If number is higher than randomNumber, the answer is "Your number is higher."
               If number is lower than randomNumber, the answer is "Your number is lower."
               If number is equal randomNumber, the answer is "Congratulation! You win."
     ***/
     string answer;
-    if (number > randomNumber) answer = "Your number is higher.";
-    else if (number < randomNumber) answer = "Your number is lower.";
-    else if (number == randomNumber) answer = "Congratulation! You win.";
+
+    if( number < randomNumber )
+        answer = "Your number is lower.";
+    else
+        if( number > randomNumber )
+            answer = "Your number is higher.";
+        else
+            answer = "Congratulation! You win.";
 
     return answer;
 }
@@ -63,7 +68,10 @@ string getAnswer(int number, int randomNumber) {
 ***/
 bool checkSuccess(string answer) {
     // TODO: return the result after checking that player guessed right or wrong
-    if ((answer == "Your number is higher.") or (answer == "Your number is lower.")) return false;
+
+    if(answer != "Congratulation! You win.")
+        return false;
+
     return true;
 }
 
@@ -76,8 +84,10 @@ bool checkSuccess(string answer) {
 ***/
 bool checkContinuePlaying(char isContinued) {
     // TODO: return result after checking player continue playing or not
-    if ((isContinued == 'y') or (isContinued == 'Y')) result = true;
-    else if ((isContinued == 'n') or (isContinued == 'N')) result = false;
+    bool result = false;
+
+    if(isContinued == 'Y' || isContinued == 'y')
+        result = true;
 
     return result;
 }
@@ -85,14 +95,15 @@ bool checkContinuePlaying(char isContinued) {
 
 /***
     Args:
-        
     Returns:
         isContinues (char) : player's choice (continue playing or not)
 ***/
 char getPlayerOpinion() {
     // TODO: Ask the player about continue playing and return the player's choice
     char isContinued;
+
     cin >> isContinued;
+
     return isContinued;
 }
 
@@ -101,7 +112,7 @@ void playGuessIt() {
     int randomNumber = generateRandomNumber();
     int number;
     string answer;
-    
+
     do {
         number = getPlayerGuess();
         answer = getAnswer(number, randomNumber);
